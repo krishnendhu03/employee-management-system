bbeb3bbe7840e74c45fe2204e29c95495dce94a3
krishnendhu03 <krishnendhupv06@gmail.com>
2026-09-17 18:23:38 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return {
+    base: "/employee-management-system/",
     plugins: [react()],
 
     define: {
